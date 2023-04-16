31f27fa20b57686d157b0e77d33a74d4fa80a59d
Zenky <bzenky@gmail.com>
2023-04-16 11:38:07 -0300

    feat: stitches / font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
+import { globalStyles } from '@/styles/globals'
 import type { AppProps } from 'next/app'
+import { Nunito } from 'next/font/google'
+
+const nunito = Nunito({
+  weight: ['400', '700'],
+  subsets: ['latin']
+})
+
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div
+      className={nunito.className}
+    >
+      <Component {...pageProps} />
+    </div>
+  )
 }
